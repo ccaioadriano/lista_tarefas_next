f5b6a884483ff2ad3e7942b0bbeed0db5ee36c21
Caio Adriano <caiobhadriano@gmail.com>
2023-12-08 16:39:48 -0300

    implementando component Card na Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import Image from 'next/image'
-
+import Image from "next/image";
+import Card from "./components/Card";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <div className="flex flex-wrap">
-     
-        <div  className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4">
-          <div className="bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-xl font-semibold mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, incidunt?</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ipsa accusamus accusantium reiciendis ut placeat ex soluta beatae assumenda illo totam impedit, at eligendi amet cumque unde rerum cupiditate ea?</p>
-          </div>
-        </div>
-    </div>
+      <div className="flex flex-wrap">
+        <Card />
+      </div>
     </main>
-  )
+  );
 }
