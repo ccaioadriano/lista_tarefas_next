7fcfd0eb2fe1067c5c22fa724a7aaffd52f59fd1
Caio Adriano <caiobhadriano@gmail.com>
2023-12-08 16:26:08 -0300

    Estruturando o projeto inicial

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-export default function Home() {
+export default function Teste() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <div className="flex flex-wrap">
