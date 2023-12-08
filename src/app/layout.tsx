ef9b63578955d382c36edc119212ac758c3c9014
Caio Adriano <caiobhadriano@gmail.com>
2023-12-08 16:39:14 -0300

    Component Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,13 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header className="bg-gradient-to-r from-rgb-background-start to-rgb-background-end p-4">
-          <div className="container mx-auto">
-            <h1 className="text-2xl text-rgb-foreground font-semibold">
-              Meu Aplicativo
-            </h1>
-          </div>
-        </header>
+        <Header />
         {children}
       </body>
     </html>
